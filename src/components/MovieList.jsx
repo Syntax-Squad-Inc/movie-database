814d4db91b8885d2616253860609fb059e4c1eb7
Thanyani266 <thakwinda66@gmail.com>
2025-01-30 15:45:45 +0200

    fixing the cards and adding toggle button

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+import { Container, Typography, Grid, Button, ButtonGroup } from '@mui/material';
 import PropTypes from 'prop-types';
-import { Grid, Container, Typography } from '@mui/material';
 import MovieCard from './MovieCard';
 
 const MovieList = ({ movies }) => {
-  if (!movies.length) {
+  const [filter, setFilter] = useState('all');
+
+  const filterMovies = (movies, filter) => {
+    switch (filter) {
+      case 'popular':
+        return [...movies].filter(a => a.popularity >= 2);
+      case 'latest':
+        return [...movies].filter(a => new Date(a.release_date).getFullYear() >= 2015);
+      default:
+        return movies;
+    }
+  };
+
+  const filteredMovies = filterMovies(movies, filter);
+
+  if (!filteredMovies.length) {
     return (
       <Container>
         <Typography variant="h5" sx={{ textAlign: 'center', mt: 4 }}>
@@ -15,8 +31,13 @@ const MovieList = ({ movies }) => {
 
   return (
     <Container>
+      <ButtonGroup sx={{ mt: 4 }} variant="outlined" aria-label="outlined button group">
+        <Button onClick={() => setFilter('all')}>All</Button>
+        <Button onClick={() => setFilter('popular')}>Popular</Button>
+        <Button onClick={() => setFilter('latest')}>Latest</Button>
+      </ButtonGroup>
       <Grid container spacing={3} sx={{ mt: 2 }}>
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
             <MovieCard movie={movie} />
           </Grid>
